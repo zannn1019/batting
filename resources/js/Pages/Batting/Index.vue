c7a59805f94d79fea522c45ec4430d384bc4d1b8
zannn1019 <fauzanguci1019@gmail.com>
2025-09-02 15:20:42 +0700

    spacing

--- resources/js/Pages/Batting/Index.vue
+++ resources/js/Pages/Batting/Index.vue
@@ -105,7 +105,7 @@ function rejectBatting(id) {
                                             {{ batting.status }}
                                         </span>
                                     </td>
-                                    <td class="px-4 py-2 border ">
+                                    <td class="px-4 py-2 border space-x-2">
                                         <!-- Detail -->
                                         <label
                                             for="detail-modal"
