bb9d3d76f72215f4f835e56175f8f86b53453d10
zannn1019 <fauzanguci1019@gmail.com>
2025-09-02 15:01:09 +0700

    team

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,46 +1,49 @@
 <template>
-  <AuthenticatedLayout>
-    <template #header>
-      <h2>{{ headerTitle }}</h2>
-    </template>
-    <div class="max-w-7xl mx-auto py-12">
-      <component
-        :is="currentDashboard"
-        :orders="submittedOrders"
-        :stats="stats"
-        :team="auth.team"
-        :opponents="opponents"
-        :submittedOrders="submittedOrders"
-      />
-    </div>
-  </AuthenticatedLayout>
+    <AuthenticatedLayout>
+        <template #header>
+            <h2>{{ headerTitle }}</h2>
+        </template>
+        <div class="max-w-7xl mx-auto py-12">
+            <component
+                :is="currentDashboard"
+                :orders="submittedOrders"
+                :stats="stats"
+                :team="team"
+                :opponents="opponents"
+                :submittedOrders="submittedOrders"
+            />
+        </div>
+    </AuthenticatedLayout>
 </template>
 
 <script setup>
-import { usePage } from '@inertiajs/vue3'
-import AdminDashboard from '@/Components/AdminDashboard.vue'
-import PanitiaDashboard from '@/Components/PanitiaDashboard.vue'
-import ManagerDashboard from '@/Components/ManagerDashboard.vue'
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { usePage } from "@inertiajs/vue3";
+import AdminDashboard from "@/Components/AdminDashboard.vue";
+import PanitiaDashboard from "@/Components/PanitiaDashboard.vue";
+import ManagerDashboard from "@/Components/ManagerDashboard.vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 
-const auth = usePage().props.auth.user
-const opponents = usePage().props.opponents
-const submittedOrders = usePage().props.submittedOrders
+const page = usePage().props;
+const auth = page.auth.user;
+const team = page.team;
+const opponents = page.opponents;
+const submittedOrders = page.submittedOrders;
+const stats = page.stats;
 
-const stats = usePage().props.stats
+console.log("Dashboard props:", page);
 
 const dashboards = {
-  admin: AdminDashboard,
-  panitia: PanitiaDashboard,
-  manager: ManagerDashboard,
-}
+    admin: AdminDashboard,
+    panitia: PanitiaDashboard,
+    manager: ManagerDashboard,
+};
 
-const currentDashboard = dashboards[auth.role]
+const currentDashboard = dashboards[auth.role];
 
 const headerTitle =
-  auth.role === 'admin'
-    ? 'Admin Dashboard'
-    : auth.role === 'panitia'
-    ? 'Panitia Dashboard'
-    : 'Team Manager Dashboard'
+    auth.role === "admin"
+        ? "Admin Dashboard"
+        : auth.role === "panitia"
+        ? "Panitia Dashboard"
+        : "Team Manager Dashboard";
 </script>
